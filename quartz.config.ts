c983dc9ee66dc22c2e0897697a6a3820050512d9
paisios-b <121258609+paisios-b@users.noreply.github.com>
2026-02-05 12:57:02 -0600

    Quartz sync: Feb 5, 2026, 12:57 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "paisios-b.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
